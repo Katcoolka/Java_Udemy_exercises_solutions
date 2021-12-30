03f8740f72aa35eba5cc80111bc977e80361b4dc
Katarína Badžanová <49408989+Katcoolka@users.noreply.github.com>
2021-12-30 21:18:53 +0100

    42 and 43 array exercises
    42 minimum element from array
    43 reverse array

---
+++ UdemyJavaMasterClassExcercises/src/com/udemy/exercise43/reverse/array/Main.java
@@ -0,0 +1,73 @@
+package com.udemy.exercise43.reverse.array;
+
+import java.util.Arrays;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int[] array = { 1, 2, 3, 4, 5 };
+		System.out.println("Array = " + Arrays.toString(array));
+
+		reverse(array);
+		System.out.println("Reversed array = " + Arrays.toString(array));
+
+	}
+
+	private static void reverse(int[] array) {
+		int maxIndex = array.length - 1;
+		int halfLength = array.length / 2;
+
+		for (int i = 0; i < halfLength; i++) {
+			int temp = array[i];
+			array[i] = array[maxIndex - i];
+			array[maxIndex - i] = temp;
+		}
+	}
+	
+	
+
+}
+/*
+ * Write a method called reverse() with an int array as a parameter.
+ * 
+ * The method should not return any value. In other words, the method is allowed
+ * to modify the array parameter.
+ * 
+ * To reverse the array, you have to swap the elements, so that the first
+ * element is swapped with the last element and so on.
+ * 
+ * For example, if the array is [1, 2, 3, 4, 5], then the reversed array is [5,
+ * 4, 3, 2, 1].
+ * 
+ * The method should first print out the newly passed in array as Array = [1, 2,
+ * 3, 4, 5]
+ * 
+ * and then once it's been reversed, print it out as Reversed array = [5, 4, 3,
+ * 2, 1]
+ * 
+ * TIP: When swapping the elements, use a variable to temporarily hold the
+ * current element.
+ * 
+ * NOTE: The method should be defined as private static.
+ * 
+ * NOTE: Do not add a main method to the solution code.
+ */
+
+//solution for udemy testing environment:
+
+/*
+import java.util.Arrays;
+public class ReverseArray {
+	private static void reverse(int[] array) {
+	    System.out.println("Array = " + Arrays.toString(array));
+		int maxIndex = array.length - 1;
+		int halfLength = array.length / 2;
+
+		for (int i = 0; i &lt; halfLength; i++) {
+			int temp = array[i];
+			array[i] = array[maxIndex - i];
+			array[maxIndex - i] = temp;
+		}
+		System.out.println("Reversed Array = " + Arrays.toString(array));
+	}
+}*/
